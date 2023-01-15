f9a629bbc78e5bd143078d8331b7c8eab162ec2f
SantiagoTonda <superorbe@gmail.com>
2023-01-15 15:15:08 -0300

    Antes de inicializar Firebase

--- src/app/service/skill.service.ts
+++ src/app/service/skill.service.ts
@@ -7,7 +7,7 @@ import { Skill } from '../model/skill';
   providedIn: 'root'
 })
 export class SkillService {
-  URL = 'http://localhost:8080/skill/';
+  URL = 'https://backend-portfolio-aqlz.onrender.com/skill/';
 
   constructor(private httpClient: HttpClient) { }
 
